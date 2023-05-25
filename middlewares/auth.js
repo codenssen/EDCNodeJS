db78ff2763a2e0f99dae56a9ad3e54366ed25262
ABT Aurelien <aurelien@abrassart.net>
2023-05-25 11:53:22 +0200

    Update middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,21 +4,6 @@ const config = require("../config");
 const usersService = require("../api/users/users.service");
 const NotFoundError = require("../errors/not-found");
 
-module.exports = (req, res, next) => {
-  try {
-    const token = req.headers["x-access-token"];
-    if (!token) {
-      throw "not token";
-    }
-    const decoded = jwt.verify(token, config.secretJwtToken);
-    req.user = decoded;
-    next();
-  } catch (message) {
-    next(new UnauthorizedError(message));
-  }
-};
-
-
 module.exports = async (req, res, next) => {
   try {
     const accessToken = req.headers["x-access-token"];
@@ -28,7 +13,7 @@ module.exports = async (req, res, next) => {
     const decodedToken = jwt.verify(accessToken, config.secretJwtToken);
 
     // 5. Modifiez le middleware d’authentification afin de faire récupérer toutes les informations d’un utilisateur et les faire passer dans « req » (et pas seulement l’id de l’utilisateur)
-    
+
     const user = await usersService.get(decodedToken.userId);
     req.user = user;
     next();
